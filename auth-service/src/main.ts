9a391edb54d77ebbb99ddc86337f3a6c702dc37c
Aleek <2023371035@uteq.edu.mx>
2026-04-16 20:02:07 -0600

    fix: puerto render

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -3,8 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);  
+  app.enableCors(); 
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0'); 
+  console.log(`Servidor Auth corriendo en el puerto: ${port}`);
 }
 bootstrap();
